7d2c8245259ff2838ef86a4f9202bd66684abc8f
Kingsley Ramos <Kingsleyramos@gmail.com>
2019-12-28 12:30:15 -0800

    Created db seeds

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Double", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Big Mac", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Whopper", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Big Carl", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Char Burger", true);
