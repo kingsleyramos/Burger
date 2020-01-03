58d83eddad2f37acc606f3e4b3b9d2fe445eee8a
Kingsley Ramos <Kingsleyramos@gmail.com>
2020-01-02 21:22:45 -0800

    edited burgers.js and added callback

--- models/burgers.js
+++ models/burgers.js
@@ -6,15 +6,19 @@ const burgers = {
             callback(res);
         });
     },
-    insert: function(burgerNAme, devoured) {
-        orm.insertOne("burgers", "burger_name", "devoured", burgerNAme, devoured);
+    insert: function(burgerNAme, devoured, callback) {
+
+        console.log("burgers.insert HIT");
+
+        orm.insertOne("burgers", "burger_name", "devoured", burgerNAme, devoured, function(res){
+            callback(res);
+        });
     },
     update: function(id, devoured, callback) {
         orm.updateOne("burgers", "devoured", devoured, "id", id, function(res){
             // console.log("burgers.update HIT");
             callback(res);
-        }
-        );
+        });
     }
 };
 
