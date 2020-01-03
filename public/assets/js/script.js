9e027edf4828e79c408df03a5b665d0f6e418cf3
Kingsley Ramos <Kingsleyramos@gmail.com>
2020-01-02 20:27:32 -0800

    added ajax call on button click

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,6 +2,18 @@ $(document).ready(function() {
 
     $(".devourBtn").on("click", function(event){
         const id = $(this).data("id");
+
+        console.log ("click");
+            // Send the PUT request.
+        $.ajax("/api/hamburger/" + id, {
+            type: "PUT"
+        }).then(function() {
+                console.log("changed to DEVOURED");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+
     });
 
 
