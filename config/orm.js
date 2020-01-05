8498945b2c30ab3f2f69a43df9f70acb4fb5ec53
Kingsley Ramos <Kingsleyramos@gmail.com>
2020-01-04 16:56:26 -0800

    removed template literals

--- config/orm.js
+++ config/orm.js
@@ -1,38 +1,48 @@
 const connection = require("./connection.js");
 
 const orm = {
-    selectAll: function(tableInput, callback) {
+    selectAll: (tableInput, callback) => {
         const queryString = "SELECT * FROM ??";
 
-        connection.query(queryString, [tableInput], function(err, result) {
+        connection.query(queryString, [tableInput], (err, result) => {
             if (err) throw err;
             callback(result);
         });
     },
-    insertOne: function(tableInput, colOneInput, colTwoInput, colOneValue, colTwoValue, callback) {
-        const queryString = `INSERT INTO ${tableInput} (${colOneInput}, ${colTwoInput}) VALUES ('${colOneValue}', ${colTwoValue})`;
+    insertOne: (tableInput, colOneInput, colTwoInput, colOneValue, colTwoValue, callback) => {
+        const queryString = "INSERT INTO ?? (??, ??) VALUES (?, ?)";
+        
+        //`INSERT INTO ${tableInput} (${colOneInput}, ${colTwoInput}) VALUES ('${colOneValue}', ${colTwoValue})`;
 
-        connection.query(queryString, function(err, result) {
+        // "INSERT INTO ?? (??, ??) VALUES (?, ?)"
+        // [tableInput, colOneInput, colTwoInput, colOneValue, colTwoValue]
+
+        connection.query(queryString, [tableInput, colOneInput, colTwoInput, colOneValue, colTwoValue], (err, result) => {
             if (err) throw err;
             callback(result);
         });
     },
-    updateOne: function(tableInput, setCol, setColVal, whereCol, whereColVal, callback) {
-        const queryString = `UPDATE ${tableInput} SET ${setCol} = ${setColVal} WHERE ${whereCol} = ${whereColVal}`;
-        connection.query(queryString, function(err, result) {
+    updateOne: (tableInput, setCol, setColVal, whereCol, whereColVal, callback) => {
+        const queryString = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
+
+        // `UPDATE ${tableInput} SET ${setCol} = ${setColVal} WHERE ${whereCol} = ${whereColVal}`
+        //[tableInput, setCol, setColVal, whereCol, whereColVal]
+
+        connection.query(queryString, [tableInput, setCol, setColVal, whereCol, whereColVal], (err, result) => {
             console.log("orm.updateOne HIT");
             if (err) throw err;
             callback(result);
         });
     },
-    delete: function(tableInput, whereCol, whereColVal, callback){
-        const queryString = `DELETE FROM ${tableInput} WHERE ${whereCol} = ${whereColVal}`;
-        connection.query(queryString, function(err, result){
+    delete: (tableInput, whereCol, whereColVal, callback) => {
+        const queryString = "DELETE FROM ?? WHERE ?? = ?";
+        connection.query(queryString, [tableInput, whereCol, whereColVal], (err, result) => {
             console.log("orm.delete HIT");
             if (err) throw err;
             callback(result);
         });
     }
 };
+
 
 module.exports = orm;
